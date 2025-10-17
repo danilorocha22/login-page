1f56d9f22a7103aa7b362488b3af60fdc3e564c7
Danilo Rocha <danilo.rochaa@gmail.com>
2025-10-17 13:22:01 -0300

    Customiza o component DefaultLogin

--- src/app/components/default-login/default-login.ts
+++ src/app/components/default-login/default-login.ts
@@ -1,11 +1,13 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-default-login',
   imports: [],
   templateUrl: './default-login.html',
-  styleUrl: './default-login.scss'
+  styleUrl: './default-login.scss',
 })
 export class DefaultLogin {
-
+  @Input() title: string = '';
+  @Input() primaryButtonText: string = '';
+  @Input() secondaryButtonText: string = '';
 }
