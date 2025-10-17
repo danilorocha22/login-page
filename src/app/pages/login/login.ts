9a7acf8ca3ff3fad8588103bdbbc9e5301fe5fba
Danilo <danilo.rochaa@gmail.com>
2025-10-17 19:11:45 -0300

    Ajuste do component DefaultLogin

--- src/app/pages/login/login.ts
+++ src/app/pages/login/login.ts
@@ -1,12 +1,21 @@
 import { Component } from '@angular/core';
 import { DefaultLogin } from "../../components/default-login/default-login";
+import { FormControl, FormGroup, FormsModule, ReactiveFormsModule, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
-  imports: [DefaultLogin],
+  imports: [DefaultLogin, ReactiveFormsModule],
   templateUrl: './login.html',
-  styleUrl: './login.scss'
+  styleUrl: './login.scss',
 })
 export class Login {
+  protected loginForm = new FormGroup({
+    email: new FormControl('', [Validators.required, Validators.email]),
+    password: new FormControl('', [Validators.required, Validators.minLength(6)]),
+  });
 
+  protected login() {
+    throw new Error('Method not implemented.');
+  }
 }
+
