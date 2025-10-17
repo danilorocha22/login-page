bbded770d007a46ac327f679d8d8f8cd60a26ee5
Danilo <danilo.rochaa@gmail.com>
2025-10-16 23:34:04 -0300

    Cria o component  DefaultLogin

---
+++ src/app/components/default-login/default-login.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-default-login',
+  imports: [],
+  templateUrl: './default-login.html',
+  styleUrl: './default-login.scss'
+})
+export class DefaultLogin {
+
+}
